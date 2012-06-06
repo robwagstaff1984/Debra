a322f05b12f3288b30c2b7b004dff9d05bef2480
Robert Wagstaff <robertwagstaff84@gmail.com>
2012-06-06 22:20:45 +1000

    settin up view controllers part2

---
+++ shmyki/InformationViewController.h
@@ -0,0 +1,13 @@
+//
+//  InformationViewController.h
+//  shmyki
+//
+//  Created by Robert Wagstaff on 6/06/12.
+//  Copyright (c) 2012 DWS Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InformationViewController : UIViewController
+
+@end
---
+++ shmyki/InformationViewController.m
@@ -0,0 +1,52 @@
+//
+//  InformationViewController.m
+//  shmyki
+//
+//  Created by Robert Wagstaff on 6/06/12.
+//  Copyright (c) 2012 DWS Limited. All rights reserved.
+//
+
+#import "InformationViewController.h"
+
+@implementation InformationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        self.title = NSLocalizedString(@"Information", nil);
+        self.tabBarItem.image = [UIImage imageNamed:@"second"];
+    }
+    return self;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
